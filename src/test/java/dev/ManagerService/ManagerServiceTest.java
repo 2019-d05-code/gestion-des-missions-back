5c6b4f5a05531079ce5c3139004f7e199ab3cdfa
Thibault <thibault.dos-anjos@lilo.org>
2019-05-23 15:50:20 +0200

    resolution types MissionDto -> Mission

--- src/test/java/dev/ManagerService/ManagerServiceTest.java
+++ src/test/java/dev/ManagerService/ManagerServiceTest.java
@@ -44,7 +44,7 @@ public class ManagerServiceTest {
 				"Bordeaux", Transport.Train);
 		newMission.setId(1);
 		Mockito.when(missionRepo.findById(newMission.getId())).thenReturn(Optional.of(newMission));
-		this.serviceMission.ajouterMission(DtoUtils.toMissionDto(newMission));
+		this.serviceMission.ajouterMission(newMission);
 		ManagerMission miss = new ManagerMission(newMission.getId(), Statut.VALIDEE);
 		MissionDto resultat = this.managerService.modifierStatut(miss);
 		Assert.assertTrue(resultat.getStatut().equals(Statut.VALIDEE));
@@ -54,7 +54,7 @@ public class ManagerServiceTest {
 	public void testForBaseDesDonne() {
 		Mission newMission = new Mission(LocalDate.now().plusDays(10), LocalDate.now().plusDays(80), "Toulouse",
 				"Bordeaux", Transport.Train);
-		this.serviceMission.ajouterMission(DtoUtils.toMissionDto(newMission));
+		this.serviceMission.ajouterMission(newMission);
 		Mockito.when(missionRepo.findById(newMission.getId())).thenReturn(Optional.of(newMission));
 	}
 
