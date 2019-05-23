27d768fa430a8b00234754e8385ff8b5760e83e0
Thibault <thibault.dos-anjos@lilo.org>
2019-05-23 17:00:19 +0200

    affichage des missions par collègue ok GET /collegue/{id}

---
+++ src/main/java/dev/controller/CollegueController.java
@@ -0,0 +1,25 @@
+package dev.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.domainDto.MissionDto;
+import dev.service.MissionService;
+
+@RestController
+@RequestMapping("/collegue")
+public class CollegueController {
+	@Autowired
+	private MissionService missionService;
+	
+	@GetMapping(path = "/{id}")
+	public List<MissionDto> afficherToutesLesMissions(@PathVariable Long id) {
+		return this.missionService.recupererMissionParCollegue(id);
+	}
+
+}
